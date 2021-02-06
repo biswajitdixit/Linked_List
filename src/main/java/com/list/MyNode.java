12607c37df59b585dfa0757a67e30fe474b3259e
biswajitdixit <buntyoec1998@gmail.com>
2021-02-06 20:11:02 +0530

    ADDED: the simple linked list created

---
+++ src/main/java/com/list/MyNode.java
@@ -0,0 +1,18 @@
+package com.list;
+
+public class MyNode <K>{
+    private  MyNode next;
+    private  K Key;
+    public  MyNode(K key){
+        this.Key=null;
+        this.next=null;
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+}
