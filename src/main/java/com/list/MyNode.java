fcbffc53c43d11ac8d6b4fe2e98eb583759265c4
biswajitdixit <buntyoec1998@gmail.com>
2021-02-06 20:32:37 +0530

    ADDED: add 30,56,70 in linked list

--- src/main/java/com/list/MyNode.java
+++ src/main/java/com/list/MyNode.java
@@ -1,18 +1,28 @@
 package com.list;
 
-public class MyNode <K>{
-    private  MyNode next;
+public class MyNode <K>implements INode<K>{
     private  K Key;
+    private  INode next;
     public  MyNode(K key){
-        this.Key=null;
-        this.next=null;
+        this.Key = key;
+        this.next = null;
     }
 
-    public MyNode getNext() {
+    @Override
+    public K getKey() {
+        return Key;
+    }
+
+    @Override
+    public void setKey(K key) {
+        this.Key=key;
+    }
+
+    public INode getNext() {
         return next;
     }
 
-    public void setNext(MyNode next) {
+    public void setNext(INode next) {
         this.next = next;
     }
 }
